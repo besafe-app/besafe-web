c75c0a01044bfcb6b61d0453294b5198bbafbc79
Ermelinda Abreu <ermelindaabreu@ioasys.com.br>
2020-04-22 18:49:01 -0300

    Deleting comments

--- src/containers/Admins/Admins.jsx
+++ src/containers/Admins/Admins.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import urls from 'utils/constants/urls';
 import Users from 'components/presentation/Users';
-// import ButtonDefault from 'components/core/ButtonDefault';
-// import { ButtonContainer } from './stylesAdmins';
 
 const Admins = () => (
   <>
     <Users />
-    {/* <ButtonContainer>
-      <Link to={urls.ROUTES.SIGNUP}>
-        <ButtonDefault
-          value="Novo Usuário"
-          size="small"
-        />
-      </Link>
-    </ButtonContainer> */}
   </>
 );
 
