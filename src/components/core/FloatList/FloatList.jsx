5571c6c89cb14c606447b88cba7b0b97615375c8
Emerson Paiva <emersonpaiva@ioasys.com.br>
2020-04-28 21:48:05 -0300

    feat(filters): init map filters components

---
+++ src/components/core/FloatList/FloatList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as S from './FloatListStyle';
+
+const FloatList = () => (
+  <S.Teste>
+    <span>1039</span>
+    <span>Sexo</span>
+    <span>Faixa etária</span>
+    <span>Condições</span>
+    <span>Outros dados</span>
+  </S.Teste>
+);
+
+export default FloatList;
