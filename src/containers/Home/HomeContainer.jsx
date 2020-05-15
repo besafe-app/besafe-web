e3a7af106793c512e4653a8f8ad58c80672bd57c
Guilherme Dutra <guidutra@Guilhermes-iMac.local>
2020-05-15 17:09:40 -0300

    InicioMapa

--- src/containers/Home/HomeContainer.jsx
+++ src/containers/Home/HomeContainer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { GoogleMap, Marker } from "react-google-maps"
+
 
 import Menu from 'components/presentation/Menu';
 import ContainerDefault from 'components/core/ContainerDefault';
@@ -6,9 +8,81 @@ import FloatList from 'components/core/FloatList';
 
 const Home = () => (
   <ContainerDefault>
-    <Menu />
-    <div>map</div>
-    <FloatList />
+    const fetch = require("isomorphic-fetch");
+const { compose, withProps, withHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} = require("react-google-maps");
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
+
+const MapWithAMarkerClusterer = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA1G6tjgGwDdsC96GcCBies7ecBYs7BImg=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withHandlers({
+    onMarkerClustererClick: () => (markerClusterer) => {
+      const clickedMarkers = markerClusterer.getMarkers()
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+      console.log(clickedMarkers)
+    },
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={3}
+    defaultCenter={{ lat: 25.0391667, lng: 121.525 }}
+  >
+    <MarkerClusterer
+      onClick={props.onMarkerClustererClick}
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.markers.map(marker => (
+        <Marker
+          key={marker.photo_id}
+          position={{ lat: marker.latitude, lng: marker.longitude }}
+        />
+      ))}
+    </MarkerClusterer>
+  </GoogleMap>
+);
+
+class DemoApp extends React.PureComponent {
+  componentWillMount() {
+    this.setState({ markers: [] })
+  }
+
+  componentDidMount() {
+    const url = [
+      // Length issue
+      `https://gist.githubusercontent.com`,
+      `/farrrr/dfda7dd7fccfec5474d3`,
+      `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
+    ].join("")
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ markers: data.photos });
+      });
+  }
+
+  render() {
+    return (
+      <MapWithAMarkerClusterer markers={this.state.markers} />
+    )
+  }
+}
+
+<DemoApp />
   </ContainerDefault>
 );
 
